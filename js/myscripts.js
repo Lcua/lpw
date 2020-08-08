a04f48bda7e8a41392aedc278d449e7aeb9d897e
Luca <luca-helmke@gmx.de>
2020-08-08 14:26:38 +0200

    commit 1

---
+++ js/myscripts.js
@@ -0,0 +1,75 @@
+var currentSlide = 1;
+var home = null;
+var music = null;
+var coding = null;
+
+console.log(currentSlide);
+
+
+
+function nextSlide() {
+    switch(currentSlide) {
+        case 1:
+            currentSlide++;
+            console.log(currentSlide);
+            slideSelect();
+            break;
+        case 2:
+            currentSlide++;
+            console.log(currentSlide);
+            slideSelect();
+            break;
+        case 3: 
+            currentSlide = 1;
+            console.log(currentSlide);
+            slideSelect();
+            break;
+    }
+}
+
+function prevSlide() {
+    switch(currentSlide) {
+        case 1:
+            currentSlide = 3;
+            console.log(currentSlide);
+            slideSelect();
+            break;
+
+        case 2:
+            currentSlide--;
+            console.log(currentSlide);
+            slideSelect();
+            break;
+        case 3:
+            currentSlide--;
+            console.log(currentSlide);
+            slideSelect();
+            break;
+    }
+}
+
+function slideSelect() {
+    switch(currentSlide) {
+        case 1:
+            document.getElementById("screenhead1").classList.replace("hide-slide", "show-slide");
+            document.getElementById("screenhead2").classList.replace("show-slide", "hide-slide");
+            document.getElementById("screenhead3").classList.replace("show-slide", "hide-slide");
+            document.getElementById("screenbot2").classList.replace("show-slide", "hide-slide");
+            document.getElementById("screenbot3").classList.replace("show-slide", "hide-slide");
+            break;
+        case 2:
+            document.getElementById("screenhead1").classList.replace("show-slide", "hide-slide");
+            document.getElementById("screenhead2").classList.replace("hide-slide", "show-slide");
+            document.getElementById("screenhead3").classList.replace("show-slide", "hide-slide");
+            document.getElementById("screenbot2").classList.replace("hide-slide", "show-slide");
+            document.getElementById("screenbot3").classList.replace("show-slide", "hide-slide");
+            break;
+        case 3:
+            document.getElementById("screenhead1").classList.replace("show-slide", "hide-slide");
+            document.getElementById("screenhead2").classList.replace("show-slide", "hide-slide");
+            document.getElementById("screenhead3").classList.replace("hide-slide", "show-slide");
+            document.getElementById("screenbot2").classList.replace("show-slide", "hide-slide");
+            document.getElementById("screenbot3").classList.replace("hide-slide", "show-slide");
+            break;
+    }
+}
